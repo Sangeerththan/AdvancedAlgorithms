334778d32876c10d8a3cca3c7cc6b4dea63578f9
sngeerththan <sangeerththan.15@cse.mrt.ac.lk>
2019-03-28 00:21:22 +0530

    Initial implementaion

---
+++ Assignment1/BST.h
@@ -0,0 +1,197 @@
+#include <stdexcept>
+#include <cstdlib>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class BST {
+public:
+    BST();
+    size_t height();
+    bool isEmpty(void) const;
+    size_t size(void) const;
+    bool contains(int key) const;
+    int get(int key) const;
+    void put(int key, int val);
+    void deleteMin(void);
+    void deleteMax(void);
+    void del(int key);
+    int min(void);
+    int max(void);
+private:
+    class Node;
+    Node* root;
+    size_t size(Node* x) const;
+    Node* get(Node* x, int key) const;
+    Node* put(Node* x, int key, int val);
+    Node* deleteMin(Node* x);
+    Node* deleteMax(Node* x);
+    Node* del(Node* x, int key);
+    Node* minNode(Node* x);
+    Node* maxNode(Node* x);
+    size_t height(Node* x);
+};
+
+struct BST::Node {
+    int key;
+    int val;
+    Node* left;
+    Node* right;
+    size_t size;
+
+    Node(int key, int val, size_t size)
+        : key(key), val(val), size(size), left(nullptr), right(nullptr) {}
+};
+    
+BST::BST() : root(nullptr) {}
+
+bool BST::isEmpty(void) const {
+    return size() == 0;
+}
+
+size_t BST::size(void) const {
+    return size(root);
+}
+
+size_t BST::size(Node* x) const {
+    if(x == nullptr) return 0;
+    return x->size;
+}
+
+bool BST::contains(int key) const {
+    return get(root, key) != nullptr;
+}
+
+int BST::get(int key) const {
+    Node* n = get(root, key);
+    if(n == nullptr) throw std::runtime_error("key not found"); 
+    return n->val;
+}
+
+BST::Node* BST::get(Node* x, int key) const {
+    if(x == nullptr) return nullptr;
+    if(key < x->key) return get(x->left, key);
+    else if(key > x->key) return get(x->right, key);
+    return x;
+}
+
+void BST::put(int key, int val) {
+    root = put(root, key, val);
+}
+
+BST::Node* BST::put(Node* x, int key, int val) {
+    if(x == nullptr) return new Node(key, val, 1);
+    if(key < x->key) {
+        x->left = put(x->left, key, val);
+    } else if(key > x->key) {
+        x->right = put(x->right, key, val);
+    } else {
+        x->val = val;
+    }
+    x->size = 1 + size(x->left) + size(x->right);
+    return x;
+}
+
+void BST::deleteMin(void) {
+    if(isEmpty()) {
+        throw new std::runtime_error("Empty tree");
+    }
+    root = deleteMin(root);
+}
+
+BST::Node* BST::deleteMin(Node* x) {
+    if(x->left == nullptr) {
+        Node* right = x->right;
+        delete x;
+        return right;
+    }
+    x->left = deleteMin(x->left);
+    x->size = size(x->left) + size(x->right) + 1;
+    return x;
+}
+
+void BST::deleteMax(void) {
+    if(isEmpty()) {
+        throw new std::runtime_error("Empty tree");
+    }
+    root = deleteMax(root);
+}
+
+BST::Node* BST::deleteMax(Node* x) {
+    if(x->right == nullptr) {
+        Node* left = x->left;
+        delete x;
+        return left;
+    }
+    x->right = deleteMax(x->right);
+    x->size = size(x->left) + size(x->right) + 1;
+    return x;
+}
+
+void BST::del(int key) {
+    root = del(root, key);
+}
+
+BST::Node* BST::del(Node* x, int key) {
+    if(x == nullptr) {
+        return nullptr;
+    }
+    if(key < x->key) {
+        x->left = del(x->left, key);
+    } else if(key > x->key) {
+        x->right = del(x->right, key);
+    } else {
+        if(x->right == nullptr) {
+            Node* left = x->left;
+            delete x;
+            return left;
+        }
+        if(x->left == nullptr) {
+            Node* right = x->right;
+            delete x;
+            return right;
+        }
+        Node* t = x;
+        x = minNode(t->right);
+        x->right = deleteMin(t->right);
+        x->left = t->left;
+    }
+    x->size = size(x->left) + size(x->right) + 1;
+    return x;
+}
+
+int BST::min(void) {
+    return minNode(root)->key;
+}
+
+BST::Node* BST::minNode(Node* x) {
+    if(x->left == nullptr) {
+        return x;
+    }
+    return minNode(x->left);
+}
+
+int BST::max(void) {
+    return maxNode(root)->key;
+}
+
+BST::Node* BST::maxNode(Node* x) {
+    if(x->right == nullptr) {
+        return x;
+    }
+    return maxNode(x->right);
+}
+
+size_t BST::height() {
+    return height(root);
+}
+
+size_t BST::height(Node* x) {
+    if(x == nullptr) {
+        return 0;
+    }
+    size_t left_h = height(x->left) + 1;
+    size_t right_h = height(x->right) + 1;
+    return left_h < right_h ? right_h : left_h;
+}
---
+++ Assignment1/SplayTree.h
@@ -0,0 +1,168 @@
+#include <stdexcept>
+#include <iostream>
+
+struct SplayTree {
+public:
+    SplayTree();
+    bool contains(int key);
+    void put(int key, int val);
+    void del(int key);
+    size_t height(void) const;
+    size_t size(void) const;
+private:
+    struct Node;
+    Node* root;
+    Node* get(int key);
+    Node* splay(Node* h, int key);
+    Node* rotateRight(Node* h);
+    Node* rotateLeft(Node* h);
+    size_t height(Node* x) const;
+    size_t size(Node* x) const;
+};
+
+struct SplayTree::Node {
+    int key;
+    int val;
+    Node* left;
+    Node* right;
+
+    Node(int key, int val) 
+        : key(key), val(val), left(nullptr), right(nullptr) {}
+};
+
+SplayTree::SplayTree() : root(nullptr) {}
+
+bool SplayTree::contains(int key) {
+    return get(key) != nullptr;
+}
+
+SplayTree::Node* SplayTree::get(int key) {
+    root = splay(root, key);
+    if(key == root->key) {
+        return root;
+    }
+    return nullptr;
+}
+
+void SplayTree::put(int key, int val) {
+    if(root == nullptr) {
+        root = new Node(key, val);
+        return;
+    }
+    root = splay(root, key);
+    if(key < root->key) {
+        Node* n = new Node(key, val);
+        n->left = root->left;
+        n->right = root;
+        root->left = nullptr;
+        root = n;
+    } else if(key > root->key) {
+        Node* n = new Node(key, val);
+        n->right = root->right;
+        n->left = root;
+        root->right = nullptr;
+        root = n;
+    } else {
+        root->val = val;
+    }
+}
+
+void SplayTree::del(int key) {
+    if(root == nullptr) {
+        return;
+    }
+    root = splay(root, key);
+    if(key == root->key) {
+        if(root->left == nullptr) {
+            root = root->right;
+        } else {
+            Node* x = root->right;
+            root = root->left;
+            splay(root, key);
+            root->right = x;
+        }
+    }
+}
+
+SplayTree::Node* SplayTree::splay(Node* h, int key) {
+    if(h == nullptr) {
+        return nullptr;
+    }
+    if(key < h->key) {
+        if(h->left == nullptr) {
+            return h;
+        }
+        if(key < h->left->key) {
+            h->left->left = splay(h->left->left, key);
+            h = rotateRight(h);
+        } else if(key > h->left->key) {
+            h->left->right = splay(h->left->right, key);
+            if(h->left->right != nullptr) {
+                h->left = rotateLeft(h->left);
+            }
+        }
+        if(h->left == nullptr) {
+            return h;
+        } else {
+            return rotateRight(h);
+        }
+    } else if(key > h->key) {
+        if(h->right == nullptr) {
+            return h;
+        }
+        if(key < h->right->key) {
+            h->right->left = splay(h->right->left, key);
+            if(h->right->left != nullptr) {
+                h->right = rotateRight(h->right);
+            }
+        } else if(key > h->right->key) {
+            h->right->right = splay(h->right->right, key);
+            h = rotateLeft(h);
+        }
+        if(h->right == nullptr) {
+            return h;
+        } else {
+            return rotateLeft(h);
+        }
+    } else {
+        return h;
+    }
+}
+
+size_t SplayTree::height(void) const {
+    return height(root);
+}
+
+size_t SplayTree::height(Node* x) const {
+    if(x == nullptr) {
+        return 0;
+    }
+    size_t left_h = height(x->left) + 1;
+    size_t right_h = height(x->right) + 1;
+    return left_h > right_h ? left_h : right_h;
+}
+
+size_t SplayTree::size(void) const {
+    return size(root);
+}
+
+size_t SplayTree::size(Node* x) const {
+    if(x == nullptr) {
+        return 0;
+    }
+    return 1 + size(x->left) + size(x->right);
+}
+
+SplayTree::Node* SplayTree::rotateRight(Node* h) {
+    Node* x = h->left;
+    h->left = x->right;
+    x->right = h;
+    return x;
+}
+
+SplayTree::Node* SplayTree::rotateLeft(Node* h) {
+    Node* x = h->right;
+    h->right = x->left;
+    x->left = h;
+    return x;
+}

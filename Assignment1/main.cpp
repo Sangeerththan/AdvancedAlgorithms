11881ec7b468b1792701f497ba98d571855003a4
sngeerththan <sangeerththan.15@cse.mrt.ac.lk>
2019-03-29 15:58:19 +0530

    Splaytree.cpp

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -6,5 +6,16 @@
 using namespace std;
 
 int main() {
+    BST::BST test = BST::BST();
+    test.put(3,4);
+    //test.put(4,5);
+    //cout "Maximum" >> test.get(3) ;
+    std::cout << test.get(3) << "\n";
+
+
+
+
+    
+
     return 0;
 }
